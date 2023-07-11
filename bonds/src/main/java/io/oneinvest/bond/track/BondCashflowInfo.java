7ca48a9a18bee15b2816998e6cf57185f14c9a0a
maxim5 <smartmaxim@gmail.com>
2023-07-11 22:33:23 +0200

    Filter cash-flows by year

--- bonds/src/main/java/io/oneinvest/bond/track/BondCashflowInfo.java
+++ bonds/src/main/java/io/oneinvest/bond/track/BondCashflowInfo.java
@@ -2,15 +2,35 @@ package io.oneinvest.bond.track;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
 public interface BondCashflowInfo {
     @NotNull List<Payment> payments();
 
+    default @NotNull List<Payment> knownPaymentsFor(int year) {
+        return payments().stream().filter(Payment::isKnown).filter(payment -> payment.year() == toCalendarYear(year)).toList();
+    }
+
+    static int toCalendarYear(int year) {
+        assert year < 1000 || year > 2000;
+        return year < 1000 ? Calendar.getInstance().get(Calendar.YEAR) + year : year;
+    }
+
     record Payment(@NotNull Date date, double value, double annualYield) {
         public Payment {
             assert value > 0 == annualYield > 0;
+        }
+
+        public @NotNull LocalDate localDate() {
+            return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        }
+
+        public int year() {
+            return localDate().getYear();
         }
 
         public boolean isKnown() {
