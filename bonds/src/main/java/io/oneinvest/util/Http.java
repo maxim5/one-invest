13499405c6139603a03691c6951094d4f14cb118
maxim <smartmaxim@gmail.com>
2023-07-13 15:50:58 +0200

    Http call: can use the cache

--- bonds/src/main/java/io/oneinvest/util/Http.java
+++ bonds/src/main/java/io/oneinvest/util/Http.java
@@ -4,6 +4,7 @@ import com.google.common.flogger.FluentLogger;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
@@ -16,6 +17,23 @@ public class Http {
                                              "(KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36";
 
     public static @NotNull String httpCall(@NotNull String url) {
+        return httpCall(url, ResponseBody::string);
+    }
+
+    public static @NotNull String httpCall(@NotNull String url, @NotNull HttpCache cache, @NotNull HttpOptions options) {
+        HttpCache.HttpEntry httpEntry = cache.get(url);
+        if (httpEntry != null && httpEntry.isFresh(options.expireMillis())) {
+            return httpEntry.contentAsString();
+        }
+        return httpCall(url, responseBody -> {
+            HttpCache.HttpEntry value = HttpCache.HttpEntry.utf8(responseBody.bytes());
+            cache.put(url, value);
+            return value.contentAsString();
+        });
+    }
+
+    private static <T> @NotNull T httpCall(@NotNull String url,
+                                           @NotNull ThrowFunction<ResponseBody, T, IOException> onSuccess) {
         OkHttpClient client = new OkHttpClient.Builder().build();
         Request request = new Request.Builder()
             .get()
@@ -26,9 +44,11 @@ public class Http {
 
         try (Response response = client.newCall(request).execute()) {
             log.at(Level.INFO).log("%s %s", response.request().url(), response.code());
-            return response.body().string();
+            return onSuccess.apply(response.body());
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
     }
+
+    public record HttpOptions(long expireMillis) {}
 }
---
+++ bonds/src/main/java/io/oneinvest/util/ThrowFunction.java
@@ -0,0 +1,25 @@
+package io.oneinvest.util;
+
+/**
+ * Represents a function that accepts one argument and produces a result
+ * and potentially throws a {@link Throwable}.
+ * <p>
+ * The {@code ThrowFunction} interface is similar to
+ * {@link java.util.function.Function}, except that a {@code ThrowFunction}
+ * can throw any kind of exception, including checked exceptions.
+ *
+ * @param <T> the type of the input to the function
+ * @param <R> the type of the result of the function
+ * @param <E> the type of Throwable thrown
+ * @see java.util.function.Function
+ */
+@FunctionalInterface
+public interface ThrowFunction<T, R, E extends Throwable> {
+    /**
+     * Applies this function to the given argument.
+     *
+     * @param t the function argument
+     * @return the function result
+     */
+    R apply(T t) throws E;
+}
