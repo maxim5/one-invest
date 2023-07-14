47f35f9755611d62047a8f5f44265f05eb373dc5
maxim <smartmaxim@gmail.com>
2023-07-14 18:02:57 +0200

    Fix the test

--- bonds/src/test/java/io/oneinvest/util/CsvTest.java
+++ bonds/src/test/java/io/oneinvest/util/CsvTest.java
@@ -9,12 +9,12 @@ import static com.google.common.truth.Truth.assertThat;
 public class CsvTest {
     @Test
     public void foo() {
-        assertThat(Stream.of("foo").collect(Csv.toCsvLine())).isEqualTo("foo");
-        assertThat(Stream.of("foo bar").collect(Csv.toCsvLine())).isEqualTo("foo bar");
-        assertThat(Stream.of("foo,bar").collect(Csv.toCsvLine())).isEqualTo("\"foo,bar\"");
+        assertThat(Stream.of("foo").collect(Csv.toCsvLine())).isEqualTo("foo\n");
+        assertThat(Stream.of("foo bar").collect(Csv.toCsvLine())).isEqualTo("foo bar\n");
+        assertThat(Stream.of("foo,bar").collect(Csv.toCsvLine())).isEqualTo("\"foo,bar\"\n");
 
-        assertThat(Stream.of("foo", "bar").collect(Csv.toCsvLine())).isEqualTo("foo,bar");
-        assertThat(Stream.of("foo bar", "baz").collect(Csv.toCsvLine())).isEqualTo("foo bar,baz");
-        assertThat(Stream.of("foo,bar", "baz").collect(Csv.toCsvLine())).isEqualTo("\"foo,bar\",baz");
+        assertThat(Stream.of("foo", "bar").collect(Csv.toCsvLine())).isEqualTo("foo,bar\n");
+        assertThat(Stream.of("foo bar", "baz").collect(Csv.toCsvLine())).isEqualTo("foo bar,baz\n");
+        assertThat(Stream.of("foo,bar", "baz").collect(Csv.toCsvLine())).isEqualTo("\"foo,bar\",baz\n");
     }
 }
